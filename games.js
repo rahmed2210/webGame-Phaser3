fd57f1750e22fd7704c7fee5ec47a2f327404855
Rubal <rahmed2210@gmail.com>
2024-12-03 14:31:36 -0500

    Web Game with phaser 3

---
+++ games.js
@@ -0,0 +1,17 @@
+// Create our only scene called mainScene, in the game.js file
+class mainScene {
+  // The three methods currently empty
+
+  preload() {
+    // This method is called once at the beginning
+    // It will load all the assets, like sprites and sounds  
+  }
+  create() {
+    // This method is called once, just after preload()
+    // It will initialize our scene, like the positions of the sprites
+  }
+  update() {
+    // This method is called 60 times per second after create() 
+    // It will handle all the game's logic, like movements
+  }
+}
